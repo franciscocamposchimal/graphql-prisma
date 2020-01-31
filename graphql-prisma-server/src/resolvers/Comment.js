ce65baa00178ef853f24bad5a19443b0b83335f1
franciscocamposchimal <camposfranciscoing@gmail.com>
2020-01-31 00:26:03 -0600

    prisma/config-getSchema

---
+++ graphql-prisma-server/src/resolvers/Comment.js
@@ -0,0 +1,12 @@
+const Comment = {
+    author(parent, args, ctx, info){
+        //parent.author
+        return "";
+    },
+    post(parent, args, ctx, info){
+        //parent.post
+        return "";
+    }
+};
+
+export { Comment as default };
